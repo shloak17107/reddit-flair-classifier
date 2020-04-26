6603c6e09828de08b9788717cd9fc269ed886db2
Shloak Aggarwal <31565271+shloak17107@users.noreply.github.com>
2020-04-26 19:51:32 +0530

    Update django website

--- reddit_flair_predictor/views.py
+++ reddit_flair_predictor/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 17, 2018'
+    },
+    {
+        'author': 'oisjeofes',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'July 13, 2019'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'reddit_flair_predictor/home.html', context)
